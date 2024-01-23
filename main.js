37a82ca9ea04822b251f3726872139ac2f2fbfda
coolcodeer <105897964+coolcodeer@users.noreply.github.com>
2024-01-22 21:24:09 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 music1 = ""
 music2 = ""
 
+leftWristx = 0
+leftWristy = 0
+rightWristx = 0
+rightWristy = 0
 function preload(){
     music1 = loadSound("music.mp3");
     music2 = loadSound("music2.mp3")
@@ -12,8 +16,28 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide()
 
+    poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotposes);
+
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
 }
+
+function modelloaded(){
+    console.log("posenet is initialzed");
+
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristx = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+        rightWristx = results[0].pose.rightWrist.x;
+        rightWristy = results[0].pose.rightWrist.y;
+    }
+}
+
